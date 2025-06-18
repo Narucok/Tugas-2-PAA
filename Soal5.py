c414aa1511d15086ef3ff6abaabfd9d4a86d23d8
Naruto Sitanggang <162136955+Narucok@users.noreply.github.com>
2025-06-18 20:28:57 +0700

    Create Soal5.py

---
+++ Soal5.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+
+        if n == 0 or k == 0:
+            return 0
+
+        if k >= n // 2:
+            total_profit = 0
+            for i in range(1, n):
+                if prices[i] > prices[i - 1]:
+                    total_profit += prices[i] - prices[i - 1]
+            return total_profit
+
+        
+        dp_buy = [-float('inf')] * (k + 1)   
+        dp_sell = [0] * (k + 1)              
+
+        for price in prices:
+            for j in range(k, 0, -1):
+                dp_sell[j] = max(dp_sell[j], dp_buy[j] + price)
+                dp_buy[j] = max(dp_buy[j], dp_sell[j - 1] - price)
+
+        return max(dp_sell)
